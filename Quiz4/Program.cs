92848245c0725b8a245cdc39cf9e3b22714d4ad9
Michelle-ZW <113384702+Michelle-ZW@users.noreply.github.com>
2024-03-28 14:42:01 -0400

    Update Program.cs, Fixed AB#161
    fixed #2

--- Quiz4/Program.cs
+++ Quiz4/Program.cs
@@ -8,6 +8,7 @@ namespace Quiz4
             //Database access has already been done on April 14, 2023 by Zhenqiao Wang
             //Today's date is March 28, 2024, the above was added based on instructions in COMP367 Quiz 4, question 4.
 
+            //another comment here for issue #2
             
             var builder = WebApplication.CreateBuilder(args);
 
